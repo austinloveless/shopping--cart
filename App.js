7e201ed2e02a41aa26f1585f2a5cc0453f23cd7c
austinloveless <35633324+austinloveless@users.noreply.github.com>
2018-04-18 20:40:25 -0600

    App.js
    Passed in props

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import CartHeader from './components/CartHeader.js'
-import CartFooter from './components/CartFooter.js'
-import CartItems from './components/CartItems.js'
+//import './App.css';
+//import CartHeader from './components/CartHeader.js'
+//import CartFooter from './components/CartFooter.js'
+//import CartItems from './components/CartItems.js'
 
 class App extends Component {
   render() {
+  const CartHeader = () => {
+    return (
+      <nav className="navbar navbar-dark bg-primary">
+    <a className="navbar-brand" href="#">Shopping Cart</a>
+  </nav>
+
+    )
+
+  }
+
+  const CartItems = () => {
+    return (
+      <div className="container">
+    <h1>Cart Items</h1>
+  </div>
+    )
+  }
+
+  const CartFooter = (props) => {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+    <a className="navbar-brand" href="#">&copy; { props.copyright }</a>
+  </nav>
+    )
+  }
+
+
     return (
       <div>
-        <CartHeader />
-        <CartFooter />
-        <CartItems />
+    <CartHeader />
+    <CartItems />
+    <CartFooter copyright="2016"/>
+
+
+
       </div>
     );
   }
