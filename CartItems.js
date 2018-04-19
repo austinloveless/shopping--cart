4883b66a7f5fce22e725bd7e4b0f2e83273bd2b5
austinloveless <35633324+austinloveless@users.noreply.github.com>
2018-04-18 18:22:49 -0600

    CartItems.js

---
+++ CartItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const CartItems = () => {
+  return (
+    <div className="container">
+  <h1>Items</h1>
+</div>
+  )
+}
+export default CartItems
